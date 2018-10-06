2e9301d4e8923ef6304be83c92cbc95f8da09ff8
UCGv2 <chrisglennii@yahoo.com>
2018-10-05 19:50:29 -0500

    functional but not safe

--- src/ArrayList.cpp
+++ src/ArrayList.cpp
@@ -3,67 +3,72 @@
 //
 
 #include "ArrayList.h"
+#include <iterator>
 #include <stdexcept>
 
 template <typename T>
-ArrayList<T>::ArrayList() : mArray(nullptr), mSize(0), mCapacity(0)
-{}
-
-template <typename T>
-ArrayList<T>::ArrayList(const uint32_t& size, const T& value) :
-mArray(new T[size]), mSize(size), mCapacity(size)
+ArrayList<T>::ArrayList()
+    : mArray(nullptr)
+    , mSize(0)
+    , mCapacity(0)
 {
-    for(size_t i = 0; i < size; ++i) {
-        mArray[i]= value;
-    }
 }
 
-template<typename T>
-ArrayList<T>::ArrayList(const ArrayList<T> &src) :
-mArray(new T[src.mSize]), mSize(src.mSize), mCapacity(src.mCapacity)
+template <typename T>
+ArrayList<T>::ArrayList(const uint32_t& size, const T& value)
+    : mArray(new T[size])
+    , mSize(size)
+    , mCapacity(size)
 {
-    for(size_t i = 0; i < src.mSize; ++i) {
-        mArray[i]= src.mArray[i];
+    for (size_t i = 0; i < size; ++i) {
+        mArray[i] = value;
     }
 }
 
 template <typename T>
-ArrayList<T>& ArrayList<T>::operator=(const ArrayList<T> &src)
-        {
-    ArrayList<T> copy (src);
+ArrayList<T>::ArrayList(const ArrayList<T>& src)
+    : mArray(new T[src.mSize])
+    , mSize(src.mSize)
+    , mCapacity(src.mCapacity)
+{
+    for (size_t i = 0; i < src.mSize; ++i) {
+        mArray[i] = src.mArray[i];
+    }
+}
+
+template <typename T> ArrayList<T>& ArrayList<T>::operator=(const ArrayList<T>& src)
+{
+    ArrayList<T> copy(src);
     mArray.swap(copy.mArray);
     mSize = copy.mSize;
     mCapacity = copy.mCapacity;
     return *this;
-        }
+}
 
-template <typename T>
-const uint32_t& ArrayList<T>::add(const T &value)
+template <typename T> const uint32_t& ArrayList<T>::add(const T& value)
 {
     if (mCapacity == 0) {
         mCapacity = 1;
         mArray.reset(new T[1]);
     }
     mSize++;
-    if(mSize>mCapacity){
+    if (mSize > mCapacity) {
         mCapacity *= 2;
         ScopedArray<T> temp(new T[mCapacity]);
         for (uint32_t i = 0; i < mCapacity; i++) {
-            if(i < mSize) {
+            if (i < mSize) {
                 temp[i] = mArray[i];
             } else {
                 temp[i] = T();
             }
         }
         mArray.swap(temp);
-
     }
-    mArray[mSize-1] = value;
+    mArray[mSize - 1] = value;
     return mCapacity;
 }
 
-template <typename T>
-const uint32_t& ArrayList<T>::add(const uint32_t &index, const T &value)
+template <typename T> const uint32_t& ArrayList<T>::add(const uint32_t& index, const T& value)
 {
     // 4 cases cap = 0, size greater than cap, index greater than cap, normal function
     if (mCapacity == 0) {
@@ -73,18 +78,18 @@ const uint32_t& ArrayList<T>::add(const uint32_t &index, const T &value)
     mSize++;
 
     // index greater than cap
-    while(index>mCapacity || mSize>mCapacity){
+    while (index > mCapacity || mSize > mCapacity) {
         mCapacity *= 2;
     }
-    if(index < mCapacity) {
+    if (index < mCapacity) {
         ScopedArray<T> temp(new T[mCapacity]);
         for (uint32_t i = 0; i < mCapacity; i++) {
             if (i < index) {
                 temp[i] = mArray[i];
-            } else if (i == index){
+            } else if (i == index) {
                 temp[i] = value;
             } else if (i > index && i < mSize) {
-                temp[i] = mArray[i-1];
+                temp[i] = mArray[i - 1];
             } else {
                 temp[i] = T();
             }
@@ -94,89 +99,95 @@ const uint32_t& ArrayList<T>::add(const uint32_t &index, const T &value)
     return mCapacity;
 }
 
-template <typename T>
-void ArrayList<T>::clear()
+template <typename T> void ArrayList<T>::clear()
 {
-    for(uint32_t i = mSize; i > 0; i--) {
+    for (uint32_t i = mSize; i > 0; i--) {
         mArray[i] = T();
         mSize--;
     }
     mArray.reset(nullptr);
 }
 
-template <typename T>
-const T& ArrayList<T>::get(const uint32_t &index) const
+template <typename T> const T& ArrayList<T>::get(const uint32_t& index) const
 {
-    if(index > mSize) {
+    if (index > mSize) {
         throw std::out_of_range("Index out of range");
-    }    else {
+    } else {
         return mArray[index];
     }
 }
 
-template <typename T>
-T& ArrayList<T>::get(const uint32_t &index)
+template <typename T> T& ArrayList<T>::get(const uint32_t& index)
 {
-    if(index > mSize) {
+    if (index > mSize) {
         throw std::out_of_range("Index out of range");
-    }    else {
+    } else {
         return mArray[index];
     }
 }
 
-template <typename T>
-T& ArrayList<T>::operator[](const uint32_t& index)
+template <typename T> T& ArrayList<T>::operator[](const uint32_t& index)
 {
     return const_cast<T&>(static_cast<const ArrayList<T>*>(this)->operator[](index));
 }
 
-template <typename T>
-const T& ArrayList<T>::operator[](const uint32_t& index) const
+template <typename T> const T& ArrayList<T>::operator[](const uint32_t& index) const
 {
     return mArray[index];
 }
 
-template <typename T>
-bool ArrayList<T>::isEmpty() const
+template <typename T> bool ArrayList<T>::isEmpty() const
 {
     return !mArray && mSize == 0;
 }
-
-
-
-
-
-
-
-template <typename T>
-T ArrayList<T>::remove(const uint32_t &index)
+template <typename T> ArrayListIterator<T> ArrayList<T>::begin()
 {
-    if(index>mCapacity||mSize==0){
+    ArrayListIterator<T> begin(mArray.get());
+    return begin;
+}
+template <typename T> ArrayListIterator<T> ArrayList<T>::end()
+{
+    ArrayListIterator<T> end(mArray.get() + mSize);
+    return end;
+}
+
+template <typename T> ArrayListConstIterator<T> ArrayList<T>::begin() const
+{
+    ArrayListConstIterator<T> cbegin(mArray.get());
+    return cbegin;
+}
+template <typename T> ArrayListConstIterator<T> ArrayList<T>::end() const
+{
+    ArrayListConstIterator<T> cend(mArray.get() + mSize);
+    return cend;
+}
+
+template <typename T> T ArrayList<T>::remove(const uint32_t& index)
+{
+    if (index > mCapacity || mSize == 0) {
         throw std::out_of_range(std::to_string(index));
     }
 
     T takeOut = mArray[index];
     mSize--;
-    for(uint32_t i = index; i < mSize; i++){
-        mArray[i] = mArray[i+1];
+    for (uint32_t i = index; i < mSize; i++) {
+        mArray[i] = mArray[i + 1];
     }
-    if (mSize==0) {
+    if (mSize == 0) {
         mArray.reset(nullptr);
     }
     return takeOut;
 }
 
-template <typename T>
-void ArrayList<T>::set(const uint32_t &index, const T &value)
+template <typename T> void ArrayList<T>::set(const uint32_t& index, const T& value)
 {
-    if (index > mSize||mSize==0) {
+    if (index > mSize || mSize == 0) {
         throw std::out_of_range(std::to_string(index));
     }
     mArray[index] = value;
 }
 
-template<typename T>
-uint32_t ArrayList<T>::size() const
+template <typename T> uint32_t ArrayList<T>::size() const
 {
     return mSize;
 }
--- src/ArrayListIter.cpp
+++ src/ArrayListIter.cpp
@@ -2,3 +2,102 @@
 // Created by fours on 9/21/2018.
 //
 
+#include "ArrayListIter.h"
+template <typename T> bool ArrayListIterator<T>::operator==(const ArrayListIterator<T>& rhs) const
+{
+    return mPtr == rhs.mPtr;
+}
+
+template <typename T> bool ArrayListIterator<T>::operator!=(const ArrayListIterator<T>& rhs) const
+{
+    return mPtr != rhs.mPtr;
+}
+template <typename T> // const cast stuff?
+T& ArrayListIterator<T>::operator*()
+{
+    return *mPtr;
+}
+template <typename T> const T& ArrayListIterator<T>::operator*() const
+{
+    return *mPtr;
+}
+template <typename T>
+ArrayListIterator<T>::ArrayListIterator(T* ptr)
+    : mPtr(ptr)
+{
+}
+template <typename T> T* ArrayListIterator<T>::operator->()
+{
+    return mPtr;
+}
+
+template <typename T> const T* ArrayListIterator<T>::operator->() const
+{
+    return mPtr;
+}
+
+template <typename T> ArrayListIterator<T>& ArrayListIterator<T>::operator++()
+{
+    ++mPtr;
+    return *this;
+}
+
+template <typename T> ArrayListIterator<T> ArrayListIterator<T>::operator++(int)
+{
+    mPtr++;
+    return *this;
+}
+template <typename T> ArrayListIterator<T>& ArrayListIterator<T>::operator--()
+{
+    --mPtr;
+    return *this;
+}
+template <typename T> ArrayListIterator<T> ArrayListIterator<T>::operator--(int)
+{
+    mPtr--;
+    return *this;
+}
+
+template <typename T> ArrayListIterator<T> ArrayListIterator<T>::operator+(int offset) const
+{
+    const_cast<ArrayListIterator<T>*>(this)->mPtr += offset;
+    return *this;
+}
+
+template <typename T> ArrayListIterator<T> ArrayListIterator<T>::operator-(int offset) const
+{
+    const_cast<ArrayListIterator<T>*>(this)->mPtr -= offset;
+    return *this;
+}
+template <typename T> int ArrayListIterator<T>::operator-(const ArrayListIterator<T>& rhs) const
+{
+    return (int)(mPtr - rhs.mPtr);
+}
+
+template <typename T> ArrayListIterator<T>& ArrayListIterator<T>::operator+=(const int& offset)
+{
+    mPtr += offset;
+    return *this;
+}
+
+template <typename T> ArrayListIterator<T>& ArrayListIterator<T>::operator-=(const int& offset)
+{
+    mPtr -= offset;
+    return *this;
+}
+
+template <typename X> ArrayListIterator<X> operator+(int offset, const ArrayListIterator<X>& iter)
+{
+    X* add = iter.mPtr + offset;
+    ArrayListIterator<X> item(add);
+    return item;
+}
+
+template <typename T> T& ArrayListIterator<T>::operator[](const int& index)
+{
+    return mPtr[index];
+}
+template <typename T> const T& ArrayListIterator<T>::operator[](int index) const
+{
+    return mPtr[index];
+}
